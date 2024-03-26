9379f3c417fb7da03a9633892990e1558a505430
Anubhav <65869678+anubhav255@users.noreply.github.com>
2024-03-26 22:44:06 +0530

    BookingDAOInterface.java

--- com/library/dl/BookingDAOInterface.java
+++ com/library/dl/BookingDAOInterface.java
@@ -4,5 +4,5 @@ import java.util.*;
 public interface BookingDAOInterface {
     public void add(BookingInterface booking) throws DAOException;
     public List<BookingInterface> getByCustomerCode(int customerCode) throws DAOException;
-    public void remove(int bookingCode) throws DAOException;
+    public void delete(int bookingCode) throws DAOException;
 }
